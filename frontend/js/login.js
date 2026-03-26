46d5920f2ddb72e5899063664088db65694d8100
auxiliartic <auxiliarticfyr@gmail.com>
2026-03-26 10:38:31 -0500

    login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,20 +3,29 @@ async function login(){
     const usuario = document.getElementById("usuario").value
     const password = document.getElementById("password").value
 
+    if (!usuario || !password) {
+    document.getElementById("error").innerText = "Campos obligatorios";
+    return;
+}
+
     try{
         const res = await fetch(`${API_URL}/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({username:parseInt(usuario), password: password})
+            body: JSON.stringify({usuario:Number(usuario), password: password})
         });
 
         const data = await res.json();
 
+        console.log("RESPUESTA: ", data);
+
         if(data.token){
             localStorage.setItem("token", data.token)
             localStorage.setItem("name", data.name)
+
+            console.log("login exitoso");
 
             window.location.href = "dashboard.html";
 
